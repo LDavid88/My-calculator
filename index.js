c9035094dc3810c0e685270ea1e640f83048f46d
David Romero <pollinxld@gmail.com>
2023-11-14 13:38:23 -0600

    show arithmetic value to display

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 display.value = "";
             } else if (button.value == "DE") {
                 display.value = display.value.toString().slice(0,-1);
+            } else if (button.value == "=") {
+                display.value = eval(display.value);
             }
         })
     })
